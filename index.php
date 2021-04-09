99e44de8ca0d55adf1f069e1cba92c7a4c0a6632
Kyousukee <38637935+Kyousukee@users.noreply.github.com>
2021-04-09 13:14:49 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Split Screen</title>
+
+      <link href="css/normalize.css" rel="stylesheet">
+      <link href="css/style.css" rel="stylesheet">
+
+      <link rel="preconnect" href="https://fonts.gstatic.com">
+      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;600&display=swap" rel="stylesheet">
+
+</head>
+<body>
+
+
+      <div class="wrapper">
+
+            <div class="side left">
+                  <div class="image dog"></div>
+                  <div class="caption">
+                        <h1>Paine</h1>
+                        <a class="button" href="#">Ir a Pagina de Paine</a>
+                  </div>
+            </div>
+
+            <div class="side right">
+                  <div class="image cat"></div>
+                  <div class="caption">
+                        <h1>Buin</h1>
+                        <a class="button" href="#">Ir a Pagina de Buin</a>
+                  </div>
+            </div>
+      </div>
+
+      
+</body>
+</html>
